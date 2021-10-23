a8a7e3429890234aa77036ad7aa4939b0f802819
edgardopina <edgardopina@yahoo.com>
2021-10-23 11:57:20 -0600

    Added Category, Product, Tag, and ProductTag models

--- models/Tag.js
+++ models/Tag.js
@@ -1,12 +1,21 @@
+const sequelize = require('../config/connection');
 const { Model, DataTypes } = require('sequelize');
 
-const sequelize = require('../config/connection.js');
 
 class Tag extends Model {}
 
 Tag.init(
   {
     // define columns
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    tag_name: {
+      id: DataTypes.STRING
+    }
   },
   {
     sequelize,
